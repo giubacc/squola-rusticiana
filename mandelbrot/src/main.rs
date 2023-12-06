c6d2f275815909ebcff8fb149434c50e256e9821
Giuseppe Baccini <giuseppe.baccini@live.com>
2023-12-06 17:34:35 +0100

    mandelbrot example
    Signed-off-by: Giuseppe Baccini <giuseppe.baccini@live.com>

---
+++ mandelbrot/src/main.rs
@@ -0,0 +1,45 @@
+use num::Complex;
+use std::str::FromStr;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,10", ','), Some((10, 10)));
+    assert_eq!(parse_pair::<i32>("10,10y", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+fn complex_square_add_loop(c: Complex<f64>) {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    loop {
+        z = z * z + c;
+    }
+}
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    return None;
+}
